39addbf577c350b4c0e9139b676182cbfcb80f79
amschel <102370012+amschel99@users.noreply.github.com>
2024-01-11 12:42:13 +0300

    Update traits.rs

--- traits.rs
+++ traits.rs
@@ -1,9 +1,10 @@
-{/*
+
+/*
 Lets say we have have newsletter struct and tweet struct and we want them to have a common method called summarize. So we can create a Trait and write the summarize signature
 then the 2 structs will implement the trait.
 
 
-*/}
+*/
 pub trait Summary {
   fn summarize(&self)->String;
 }
@@ -15,7 +16,7 @@ pub struct NewsLetter{
 }
 impl Summary for NewsLetter{
   fn summarize(&self)->String{
-    format!("{}, by {} ({})", self.headline, self.author, self.location);
+    format!("{}, by {} ({})", self.headline, self.author, self.location)
     
   }
 }
@@ -30,4 +31,15 @@ impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
+}
+
+fn main (){
+  let tweet=Tweet{
+    username:String::from("Amschel"),
+    content:String::from("I write Rust btw"),
+    reply:false,
+    retweet:false
+  };
+  println!("{}", tweet.summarize())
+
 }
