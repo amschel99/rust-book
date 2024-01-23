e17ada34e8f3fe0c32053e6c27556a6116c0c9e9
amschel <102370012+amschel99@users.noreply.github.com>
2024-01-23 08:08:32 +0300

    Update lifetimes.rs

--- lifetimes/lifetimes.rs
+++ lifetimes/lifetimes.rs
@@ -66,3 +66,25 @@ fn longest<'a, 'b>(x:&'a str, y:&'b str)->&'a str{
 x
 }
 */
+
+//Lifetime Elision rules
+/*
+This are some rules that the compiler follows to try guess the lifetime of references if you have not specified
+The first rule is that the compiler assigns a lifetime parameter to each parameter that’s a reference. 
+In other words, a function with one parameter gets one lifetime parameter: fn foo<'a>(x: &'a i32);
+a function with two parameters gets two separate lifetime parameters:
+fn foo<'a, 'b>(x: &'a i32, y: &'b i32); and so on.
+
+
+The second rule is that, if there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters:
+fn foo<'a>(x: &'a i32) -> &'a i32.
+
+The third rule is that, if there are multiple input lifetime parameters, 
+but one of them is &self or &mut self because this is a method, the lifetime of self is assigned 
+to all output lifetime parameters. 
+This third rule makes methods much nicer to read and write because fewer symbols are necessary.
+
+
+
+
+*/
